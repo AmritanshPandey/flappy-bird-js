cee7d8091f8fcb865df46d107a0c8f28e4b0860a
Amritansh Pandey <amritansh.pandey6@gmail.com>
2019-10-29 15:26:27 +0530

    Game Sprite Setup

--- game.js
+++ game.js
@@ -1,17 +1,61 @@
+// Linking the Canvas by id
 const cvs = document.getElementById("bird");
+
+//declaring the context of the canvas as 2d
 const ctx = cvs.getContext("2d");
 
+// Variables and Constant of the game
+let frames = 0;
 
+// Loading Sprite Image
+const sprite = new Image();
+sprite.src = "img/sprite.png"
+
+// Draw function to draw on canvas
 function draw(){
     ctx.fillStyle = "#70c5ce";
     ctx.fillRect( 0, 0, cvs.width, cvs.height );
+}
 
-
+// Update for updating the game status
+function update(){
 
 }
 
+
+// Importing Sprites from the Source
+
+// Background Image
+
+
+// Bird Sprite
+const bird = {
+    animation : [
+        { sX : 276, sY : 112 },
+        { sX : 276, sY : 139 },
+        { sX : 276, sY : 164 },
+        { sX : 276, sY : 139 },
+    ],
+
+    x : 50,
+    y : 150,
+    w : 34,
+    h : 26,
+    frame : 0,
+    draw : function(){
+        let bird = this.animation[this.frame];
+        ctx.drawImage(sprite, bird.sX,)
+    }
+
+}
+
+const 
+
+// For Loop to keep updating the game
 function loop(){
+    update();
     draw();
+    frames++;
 
     requestAnimationFrame(loop);
 }
